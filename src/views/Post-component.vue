c642a924fc48e05158ccb6e81e707c033dd69aea
Postnov Daniil <dan.postnov@gmail.com>
2019-01-27 23:15:58 +0300

    add post-component

---
+++ src/views/Post-component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="post">
+        <p>{{ post.name }}</p>
+        <p>А доставка - Додо пицца</p>
+        <footer>
+            <a target="_blank" :href="post.link">{{post.author}}</a>
+            button
+        </footer>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'post-component',
+        props: ['post'],
+    }
+</script>
