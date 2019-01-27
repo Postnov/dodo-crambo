3f2ea800efcb76b966912f72a1b9f144e5406dec
Postnov Daniil <dan.postnov@gmail.com>
2019-01-27 11:09:13 +0300

    change vars, add method send data in firebase

--- src/views/Add-post.vue
+++ src/views/Add-post.vue
@@ -1,30 +1,53 @@
 <template>
     <div>
         <h1>Добавить рифму</h1>
-        <input type="text" v-model="rhyme" placeholder="Название рифмы">
-        <input type="text" v-model="link" placeholder="Ваше имя">
-        <input type="text" v-model="name" placeholder="Ссылка на страницу вк">
+        <form @submit.prevent="sendRhyme()">
+            <input type="text" v-model="name" placeholder="Название рифмы">
+            <input type="text" v-model="author" placeholder="Ваше имя">
+            <input type="text" v-model="link" placeholder="Ссылка на страницу вк">
 
-        <button @click="sendRhyme()">Отправить</button>
+            <button type="submit">Отправить</button>
+        </form>
 
     </div>
 </template>
 
 <script>
+import { firebase } from '../main'
+
 
 export default {
     name: 'add-post',
     data() {
         return {
-            rhyme: '',
+            name: '',
             link: '',
-            name: ''
+            author: ''
 
         }
     },
     methods: {
+        makeid(amountSymbols = 50) {
+            var text = "";
+            var possible = "Aasd1231kj2nelaksdo3121321k1elaksnbfaoiuenriu23n4elkqeBCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for (var i = 0; i < amountSymbols; i++)
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        },
         sendRhyme() {
-            console.log('send')
+            firebase.firestore().collection('data').doc('rhymes').collection('published').add({
+                createdAt: new Date(),
+                id: this.makeid(),
+                author: this.author,
+                name: this.name,
+                link: this.link
+            });
+
+            this.name = '';
+            this.author = '';
+            this.link = '';
         }
     }
 }
