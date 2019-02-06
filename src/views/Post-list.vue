f099ad10c65bfff1a3ef77c176a045a82db9ecb9
Postnov Daniil <dan.postnov@gmail.com>
2019-02-06 17:08:05 +0300

    change height for show more in posts-lists

--- src/views/Post-list.vue
+++ src/views/Post-list.vue
@@ -37,7 +37,7 @@ export default {
     },
     methods: {
         handleScroll () {
-            var masonryHeight = this.$refs.masonry.$el.clientHeight,
+            var masonryHeight = this.$refs.masonry.$el.clientHeight + 150,
                 offetHeight = window.innerHeight + window.scrollY;
 
             if (offetHeight > masonryHeight) {
