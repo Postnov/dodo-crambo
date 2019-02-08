cf3349833f097ad2d60f269f869cbdf76df10043
Postnov Daniil <dan.postnov@gmail.com>
2019-02-08 12:07:29 +0300

    change create icon id

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -94,7 +94,7 @@ var Posts = {
             var width, height, left, id, random, randomLeft, top, animationDuration;
 
             //set id and random
-            id = "icon_" + Math.random().toString(16).slice(2);
+            id = "icon_" + (new Date()).getTime();
             random = this.randomNumber(1, 7, 0);
             randomLeft = this.randomNumber(0, 99, 0);
 
